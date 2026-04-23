ebeb2b5a278f2479a4981f040683f5b40abb0e9b
Steven Hawking <jonathan.bittan9@gmail.com>
2026-04-23 22:22:35 +0300

    third

--- js/script.js
+++ js/script.js
@@ -4,16 +4,16 @@ text()
 easter_eggs()
 function text(){
     document.getElementById("navbar").textContent = "steam and stem";
-    document.getElementById("navbar").onclick = function(){
+        }
+    
+    function easter_eggs(){
+        document.getElementById("navbar").onclick = function(){
         if (check_navbar_clicks === 1){
             document.getElementById("easter_egg").textContent = "you found an easter egg";
             easter_egg_count+=1
             check_navbar_clicks+=1
         } else {    
             document.getElementById("easter_egg").textContent = "You earned this easter egg!";
+    }
         }
     }
-    function easter_eggs(){
-        
-    }
-}
