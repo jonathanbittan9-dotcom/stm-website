1a0bed5c30d2a1c0d33d64623e8a6276cbcb6d68
Steven Hawking <jonathan.bittan9@gmail.com>
2026-04-24 17:20:16 +0300

    commit+

--- js/script.js
+++ js/script.js
@@ -1,19 +1,29 @@
 let check_navbar_clicks = 1
 let easter_egg_count = 0
-text()
+text_navbar()
 easter_eggs()
-function text(){
-    document.getElementById("navbar").textContent = "steam and stem";
-        }
-    
+homepage_text()
+function text_navbar(){
+    document.getElementById("homepage").textContent = "homepage";
+    document.getElementById("navbar").prepend("steam and stem");
+ }
+
+function homepage_text(){
+    document.getElementById("title").textContent = '"the best server ever created"';
+    document.getElementById("discord").textContent = "Discord server:";
+}
     function easter_eggs(){
         document.getElementById("navbar").onclick = function(){
+        const egg = document.getElementById("easter_egg");
         if (check_navbar_clicks === 1){
-            document.getElementById("easter_egg").textContent = "you found an easter egg";
+            egg.textContent = "you found an easter egg";
             easter_egg_count+=1
             check_navbar_clicks+=1
-        } else {    
-            document.getElementById("easter_egg").textContent = "You earned this easter egg!";
+        } else {
+            egg.textContent = "You earned this easter egg!";
     }
+        egg.classList.remove("pop");
+        void egg.offsetWidth;
+        egg.classList.add("pop");
         }
     }
